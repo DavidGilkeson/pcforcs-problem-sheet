5ec968d1ada1752fd411f3f30dad38c6ff829d41
David Gilkeson <davidgilkeson8@gmail.com>
2021-01-27 22:16:50 +0000

    Created bmi.py file

---
+++ bmi.py
@@ -0,0 +1,9 @@
+# 1. Write a program that calculates somebody's Body Mass Index (BMI).
+
+# The inputs are the person's height in centimetres and weight in kilograms.
+# The output  is their weight divided by their height in metres squared.
+
+# Enter weight: 65
+# Enter height: 180
+# BMI is 20.06.
+
