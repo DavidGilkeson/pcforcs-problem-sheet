fbd2dc178a4ffd96a2a4612960a43edc0d842e16
David Gilkeson <davidgilkeson8@gmail.com>
2021-02-03 16:30:00 +0000

    bitcoin problem 2 added

---
+++ bitcoin.py
@@ -0,0 +1,33 @@
+# Author: David Gilkeson
+
+# 1. Write a program (bitcoin.py) that outputs the current bitcoin price in US Dollars. You may use the code snippet below to get a Dict object that contains the price.
+# 2. Extra output all the price in the three currencies, in a neat way.
+
+import requests
+# import json
+
+# Code that pulls data from the coin desk api and returns it in JSON 
+url = "https://api.coindesk.com/v1/bpi/currentprice.json"
+returnedData = requests.get(url)
+bitCoinDict = returnedData.json()
+# print(json.dumps(bitCoinDict, indent = 4)) # This formats the code so it can be easily understood
+
+# This stores the rate of the 3 currencies
+usd = bitCoinDict["bpi"]["USD"]["rate"]
+eur = bitCoinDict["bpi"]["EUR"]["rate"]
+gbp = bitCoinDict["bpi"]["GBP"]["rate"]
+
+# Using f strings to format the code and print it to the console
+print(f'USD rate today is ${usd}')
+print(f'EUR rate today is €{eur}')
+print(f'GBP rate today is £{gbp}')
+
+
+# References
+
+
+# https://www.w3schools.com/python/python_json.asp
+
+# https://www.w3schools.com/python/python_dictionaries.asp
+
+# https://stackoverflow.com/questions/43488137/how-to-do-a-dictionary-format-with-f-string-in-python-3-6
