a1fe259673ec082a624fde326c9694e324ed909b
David Gilkeson <davidgilkeson8@gmail.com>
2021-02-15 00:25:48 +0000

    squareroot problem added

---
+++ squareroot.py
@@ -0,0 +1,22 @@
+# Author: David Gilkeson
+# Write a program that takes a positive floating-point number as input and outputs an approximation of its square root.
+
+# You should create a function called <tt>sqrt</tt> that does this.
+# I am asking you to create your own sqrt function and not to use the built in functions x ** .5 or math.sqrt(x)
+# I suggest that you look at the newton method at estimating square roots
+
+# Please enter a positive number: 14.5
+# The square root of 14.5 is approx. 3.8.
+
+number = abs(float(input("Please enter a positive number: "))) # Takes positive number from the user, stores it in a variable called number and converts it to a float. abs makes sure it is a positive number.
+print(number)
+
+# def sqrt():
+  
+
+
+
+
+# References
+
+# GeeksforGeeks. 2021. abs() in Python - GeeksforGeeks. [ONLINE] Available at: https://www.geeksforgeeks.org/abs-in-python/. [Accessed 14 February 2021].
