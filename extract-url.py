bf1c7e05c8fb0673f2ca2680979d10a23b50acc1
David Gilkeson <davidgilkeson8@gmail.com>
2021-03-04 23:33:21 +0000

    Attempt 1

--- extract-url.py
+++ extract-url.py
@@ -28,8 +28,27 @@
 # ]
 
 
+
+import re
+
+filename = "sample.log"
+path = ""
+
+with open (path + filename, "r") as logfile:
+  count = 0
+  for line in logfile:                            # Loops through the log file
+    regex = ''                                    # Stores the regex
+    url = re.findall(regex, line)                 # Uses the findall method and stores it in url variable
+    print(url[0])                                 # Prints out a list of URLs
+    count += 1                                    # Adds one to the count each time
+    
+print("fin ", count)
+
+
 # References
 
-# Real Python. 2021. Regular Expressions: Regexes in Python (Part 1) – Real Python. [ONLINE] Available at: https://realpython.com/regex-python/. [Accessed 03 March 2021].
+# GeeksforGeeks. 2021. Python | Check for URL in a String - GeeksforGeeks. [ONLINE] Available at: https://www.geeksforgeeks.org/python-check-url-string/. [Accessed 04 March 2021].
 
 # Python RegEx. 2021. Python RegEx. [ONLINE] Available at: https://www.w3schools.com/python/python_regex.asp. [Accessed 03 March 2021]
+
+# Real Python. 2021. Regular Expressions: Regexes in Python (Part 1) – Real Python. [ONLINE] Available at: https://realpython.com/regex-python/. [Accessed 03 March 2021].
